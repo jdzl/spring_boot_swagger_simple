79ba4b764f57d0a630637b0816b684bbaab616ca
david zambrano <ggmortum@gmail.com>
2018-10-25 14:24:25 -0500

    r commit

--- src/main/java/com/jdzl/config/SwaggerConfig.java
+++ src/main/java/com/jdzl/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.infotech.swagger.controller"))
+				.apis(RequestHandlerSelectors.basePackage("com.jdzl.controllers"))
 				.paths(PathSelectors.regex("/users.*"))
 				.build();
 
